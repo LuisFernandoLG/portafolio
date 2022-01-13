e38d97651cfba5e7a715675f69ce6895f582edbb
Luis Lopez <luislopez1099lg@gmail.com>
2022-01-13 16:24:29 -0700

    feat(Root): data proyects loaded from js

---
+++ app/js/data.js
@@ -0,0 +1,40 @@
+export const proyects = [
+  {
+    id: 1,
+    name: "gsp cabo landing Page",
+    link: "https://gspcabo.netlify.app/",
+    thumbail: "./dist/assets/img/proyects/gspcabo.png",
+  },
+  {
+    id: 2,
+    name: "Emoji App",
+    link: "https://admiring-kalam-5cd446.netlify.app",
+    thumbail: "./dist/assets/img/proyects/emojiapp.png",
+  },
+  {
+    id: 3,
+    name: "Type And Type",
+    link: "https://typing-applg.netlify.app",
+    thumbail: "./dist/assets/img/proyects/typeandtype.png",
+  },
+  {
+    id: 4,
+    name: "Planet fact",
+    link: "https://planet-factss.netlify.app",
+    thumbail: "./dist/assets/img/proyects/planetsSite.png",
+  },
+
+  {
+    id: 5,
+    name: "Linked List",
+    link: "https://lista-enlazada.netlify.app",
+    thumbail: "./dist/assets/img/proyects/linkedList.png",
+  },
+
+  {
+    id: 6,
+    name: "GSP Form",
+    link: "https://admiring-kalam-5cd446.netlify.app",
+    thumbail: "./dist/assets/img/proyects/gspform.png",
+  },
+];
--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,18 @@
-console.log('HELLO');
+import { proyects } from "./data";
 
-const test = () => {
-	console.log('this is a test');
+const loadProyects = () => {
+  let $proyectsContainer = document.querySelector(".proyects");
+
+  let $proyectsLoaded = "";
+  proyects.forEach(({ link, name, id, thumbail }) => {
+    $proyectsLoaded += `<li class="proyects__proyect">
+	  <a href="${link}" target="_blank">
+		<img src="${thumbail}" alt="${name}" />
+	  </a>
+	</li>`;
+  });
+
+  $proyectsContainer.innerHTML = $proyectsLoaded;
 };
+
+loadProyects();
